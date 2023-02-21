b036db15b1deb2a92b1bea35221e8693ae99dbbf
James Aules <james_aules@icloud.com>
2023-02-21 13:01:35 -0500

    First commit - Data.js

---
+++ src/components/Data.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Data = () => {
+    return(
+        <div className="Data-Box" >
+            <img className="Img-box"
+                  src={require('')}
+                  alt={""}/>
+        </div >
+    )
+}
+
+export default Data
