24d89e1b987304edf3da7eb03621a9c1dca402c2
James Aules <james_aules@icloud.com>
2023-02-22 16:40:24 -0500

    Adding props & implementing Data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ function App() {
         <h1>
           Here is what our alumni say about freeCodeCamp:
         </h1>
-        <Data />
+        <Data
+          name="Shawn Wang"
+          country="Singapore"
+          pic="shawn"
+          occupation="Software Engineer"
+          company="Amazon"
+          testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life." 
+        />
+        <Data
+          name="Sarah Chima"
+          country="Nigeria"
+          pic="sarah"
+          occupation="Software Engineer"
+          company="ChatDesk"
+          testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." 
+        />
+        <Data
+          name="emma"
+          country="Sweden"
+          pic="emma"
+          occupation="Software Engineer"
+          company="Spotify"
+          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
       </div>
     </div>
   );
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,16 +1,15 @@
-import React from "react";
 import '../styles-sheet/Data.css'
 
-const Data = () => {
+const Data = (props) => {
     return(
         <div className="data-box" >
             <img className="img-box"
-                  src={require('../images/emma.png')}
-                  alt="user-pic"/>
+                  src={require(`../images/${props.pic}.png`)}
+                  alt={props.name}/>
             <div className="testimony">
-                <p className="user-name" > Emma Bostian in Sweden</p>
-                <p className="user-occupation">"Software Engineer at Spotify</p>
-                <p className="user-testimony">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+                <p className="user-name" > <strong>{props.name}</strong> in {props.country}</p>
+                <p className="user-occupation">{props.occupation} at <strong>{props.company}</strong></p>
+                <p className="user-testimony">"{props.testimony}"</p>
             </div>
         </div >
     )
